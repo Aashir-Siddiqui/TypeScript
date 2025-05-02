f94238d37379d0da9a7c4dd61e27eeccd608a436
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-05-02 23:18:02 +0500

    data types practice

---
+++ Data_Types/index01.js
@@ -0,0 +1,52 @@
+"use strict";
+// number data type 
+let n = 10;
+n = 20;
+console.log(n);
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(10, 20));
+let num = 100;
+console.log(num.toString());
+let stuName = prompt("Enter your name");
+let rollNum = prompt("Enter any roll number");
+let nameRegex = /^[A-Za-z\s]+$/;
+if (stuName && nameRegex.test(stuName) && Number(rollNum) <= 10) {
+    console.log(`${stuName}, your class number is 1`);
+}
+else if (stuName && nameRegex.test(stuName) && Number(rollNum) > 10) {
+    console.log(`${stuName}, your class number is 2`);
+}
+else {
+    console.log(`Invalid credentials`);
+}
+let nanValue = NaN;
+if (isNaN(nanValue)) {
+    console.log("value is NaN");
+}
+// string data type
+let firstName = "Aashir ";
+let lastName = "Siddiqui";
+let fullName = firstName + lastName;
+console.log(fullName);
+let str1 = "The rain in SPAIN stays mainly in the plain";
+console.log(str1.match(/ain/gi)?.join(" "));
+console.log(str1.replace(/SPAIN/gi, "Pakistan"));
+console.log(str1.toUpperCase());
+console.log(str1.toLowerCase());
+let str2 = "Hello";
+let str3 = "World";
+const isEqual = str2 === str3;
+console.log(isEqual);
+// Boolean data type 
+let workDone = true;
+let myNameisAhmed = false;
+function isEven(a) {
+    return a % 2 === 0;
+}
+console.log(isEven(6));
+function func(b) {
+    return (b % 4 && b % 8) === 0;
+}
+console.log(func(16));
